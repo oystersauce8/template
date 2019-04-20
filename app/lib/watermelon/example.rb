cc76ec409318ec4f6d0184cc1f1d18921aa8e5ac
Travis CI User <travis@example.org>
2019-04-20 20:33:28 +0000

    Added lib

---
+++ app/lib/watermelon/example.rb
@@ -0,0 +1,15 @@
+module Watermelon
+  module Example
+    def self.example1
+      a = "example1"
+      b = nil
+      a
+    end
+
+    def self.example2
+      a = "example2"
+      b = nil
+      a
+    end
+  end
+end
