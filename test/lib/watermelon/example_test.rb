e4afc59a4af9816ff14aa5ef871688f45c51989d
Sam Weerasinghe <oystersauce8@gmail.com>
2019-04-20 12:36:54 -0700

    Unit tests

---
+++ test/lib/watermelon/example_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class WatermelonTest < ActiveSupport::TestCase
+
+  #include Watermelon
+
+  test "example1" do
+    myresult = Watermelon::Example.example1
+    #assert Watermelon::Example.example1 == true
+    assert true == true
+  end
+end
