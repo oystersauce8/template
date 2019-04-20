6a289d4a57e05361d37201b9541a7a15ff36e488
Travis CI User <travis@example.org>
2019-04-20 20:11:54 +0000

    Added bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def test_bootstrap
+  end
+
+  def test_pundit
+  end
+
+  def test_iconfonts
+  end
+
+  def test_lib
+  end
+
+  def test_mail
+  end
+
+  def test_react
+  end
+end
