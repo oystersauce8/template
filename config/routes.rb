517fb2c0d2c45f64252b3b2c45c22331c22cd22c
Sam Weerasinghe <oystersauce8@gmail.com>
2019-04-20 21:29:11 -0700

    Added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/test_bootstrap'
+  get 'pages/test_pundit'
+  get 'pages/test_iconfonts'
+  get 'pages/test_lib'
+  get 'pages/test_mail'
+  get 'pages/test_react'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
