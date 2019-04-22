16af022345eba772ed873460041211d4320f4b4e
Sam Weerasinghe <oystersauce8@gmail.com>
2019-04-22 13:00:18 -0700

    Added bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  def test_bootstrap
+  end
+
+  def test_pundit
+  end
+
+  def test_iconfonts
+  end
+
+  def test_lib
+  end
+
+  def test_mail
+  end
+
+  def test_react
+  end
+
+  def test_authentication_1
+  end
+
+  def test_authentication_2
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/test_bootstrap'
+  get 'pages/test_pundit'
+  get 'pages/test_iconfonts'
+  get 'pages/test_lib'
+  get 'pages/test_mail'
+  get 'pages/test_react'
+  get 'pages/test_authentication_1'
+  get 'pages/test_authentication_2'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
