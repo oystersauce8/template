aebf825e46e26999b6c11ed083eb0ce58f209f85
Sam Weerasinghe <oystersauce8@gmail.com>
2019-04-23 21:22:14 -0700

    Added bootstrap

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,37 @@
+class PagesController < ApplicationController
+  def test_bootstrap
+  end
+
+  def test_pundit
+  end
+
+  def test_iconfonts
+  end
+
+  def test_lib
+  end
+
+  def test_mail
+  end
+
+  def test_react
+  end
+
+  def test_authentication_1
+  end
+
+  def test_authentication_2
+  end
+
+  def test_api_1
+  end
+
+  def test_api_2
+  end
+
+  def test_api_3
+  end
+
+  def test_api_4
+  end
+end
