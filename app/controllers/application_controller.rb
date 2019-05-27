73a81d4a7115988d6ae3d533e581aafbc11bcf2c
Travis CI User <travis@example.org>
2019-05-27 14:16:03 +0000

    Added Grape

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  before_action :authenticate_user_from_token!
+
+private
+  def authenticate_user_from_token!
+    return unless request.headers["Authorization"].present?
+    user = User.find_by_token(request.headers["Authorization"]) rescue nil
+    if user
+      sign_in user
+    end
+  end
 end
