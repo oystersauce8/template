6d9ca84819ab80074f004f3f9b81731fc1c877f1
Travis CI User <travis@example.org>
2019-07-16 16:08:53 +0000

    Added lib

---
+++ app/lib/watermelon/example.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Watermelon
+  module Example
+    # This is the first submodule in the root namespace
+    def example1
+      a = 'example1'
+      b = a
+      b
+    end
+
+    def example2
+      a = 'example2'
+      b = a
+      b
+    end
+  end
+end
