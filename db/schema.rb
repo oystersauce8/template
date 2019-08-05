6f14f0981a77df2a38a66ba96ca19e3964bfff9c
Nightly (via Travis Cron) <travis@example.org>
2019-08-05 14:26:16 +0000

    Updates from 20190805-1565014907 branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_140629) do
+ActiveRecord::Schema.define(version: 2019_08_05_142515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
